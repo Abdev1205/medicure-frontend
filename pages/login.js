9f1499aad43a433d79e1fefd40ca972a08351bcb
abhay <abhaym1205@gmail.com>
2024-04-24 23:58:06 +0530

    created login page

---
+++ pages/login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '@/components/common/Navbar/Navbar'
+
+const Login = () => {
+  return (
+    <div className=' w-[100%] h-[100vh]  ' >
+      <Navbar />
+      <h1>Hello from login</h1>
+    </div>
+  )
+}
+
+export default Login
